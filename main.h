4fc7a4f2b4bf2da455a8c01b42383694b0544ab5
siphonzama <sipho1728@gmail.com>
2023-07-25 04:28:18 -0700

    printf

--- main.h
+++ main.h
@@ -1,6 +1,27 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
-int _printf(const char *format, ...);
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
-#endif
+/* flag */ 
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8 
+#define F_SPACE 16
+
+/* size */ 
+
+#define S_LONG 2
+#define S_SHORT 1
+
+struct cat
+{
+	char cat
+	int(*ct)(va_list, char[], int, int, int, int, int,);
+};
